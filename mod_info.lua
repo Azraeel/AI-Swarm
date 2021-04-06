413e93c9741a202383ead1b244d6952391e554bf
Azraeel <45247351+Azraeel@users.noreply.github.com>
2021-04-06 07:38:41 -0500

    oops

--- mod_info.lua
+++ mod_info.lua
@@ -1,5 +1,5 @@
 name = "AI-Swarm"
-uid = "a13ac591-b2d9-4d94-0230-3d4b3cce6b97"
+uid = "a13ac591-b2d9-4d94-0231-3d4b3cce6b97"
 version = 231
 copyright = "2020 Azraeel"
 description = "Welcome to Swarm. What is Swarm? Swarm is a AI Improvement Mod, Swarm wants to make the AI; Smart and Tough to be beat with the goal of reaching a level of truly Competitive AI. That Can Teach New Players and Help Everyone Get Better. REQUIRES AI-Uveso for Map Marker Generator!!!! 219 Version - 2021 Rainbow Cup III Tournment Special Update!"
